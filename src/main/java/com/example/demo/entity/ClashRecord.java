201a468841619bfface510af9a2f7b4ca6e0f7ba
bala-amypo <bala@amypo.in>
2025-12-26 15:39:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "clash_record")
 public class ClashRecord {
 
     @Id
@@ -11,8 +12,14 @@ public class ClashRecord {
 
     private String severity;
 
+    private boolean resolved;
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getSeverity() {
@@ -21,5 +28,13 @@ public class ClashRecord {
 
     public void setSeverity(String severity) {
         this.severity = severity;
+    }
+
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        this.resolved = resolved;
     }
 }
