e3cd8120f26f21d695c93aa68cae54f399d4c1d1
bala-amypo <bala@amypo.in>
2025-12-27 04:46:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -8,4 +8,8 @@ import java.util.Optional;
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     Optional<UserAccount> findByUsername(String username);
+
+    Optional<UserAccount> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
